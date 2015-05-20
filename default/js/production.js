e0a0ac04ab69566f7d3d204528148e0ba6994927
DWMiller <david.miller8@mohawkcollege.ca>
2015-05-20 16:55:43 -0400

    blocked enter key triggering covert deploy/recall

--- default/js/production.js
+++ default/js/production.js
@@ -16764,9 +16764,25 @@ dmf.registerModule('mailwrite', function(c, config) {
 dmf.registerModule('operations', function(c, config) {
     'use strict';
 
+    var operationPanels;
+
     function start() {
+        operationPanels = $('.covert-operation-panel');
+
+        operationPanels.on('keypress', function(event) {
+            if (event.keyCode == 13) {
+                //Prevent enter key from triggering form submit
+                //as it is unclear if the desired action is deploy or recall
+                event.preventDefault();
+            }
+        });
+
 
     }
+
+
+
+
 
     return {
         start: start
