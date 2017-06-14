0b8e0eb195e0c8e24ad7b70e4e70b34d5cebc05f
David Miller <davidabc66@gmail.com>
2017-06-14 03:13:13 -0400

    stuff

--- js/pages/all.js
+++ js/pages/all.js
@@ -1,24 +1,14 @@
 (function () {
     var elements = {
-        $body: $("body"),
-        $drawers: $(".drawer"),
-        $html: $("html"),
-        // $menuToggle: $("#menu-toggle"),
-        $pageContent: $("#content-page")
-        // $sidebar: $("#sidebar")
+        // $body: $("body"),
+        // $drawers: $(".drawer"),
+        // $pageContent: $(".page-content")
     };
 
-    function setUIHandlers() {
-        // elements.$menuToggle.on("click touchstart", toggleSideMenu);
-
-        // elements.$sidebar
-        //   .find(".menu-item")
-        //   .hover(menuDropdownHoverOn, menuDropdownHoverOff);
-
-        elements.$body.on("click touchstart", ".drawer-toggle", toggleDrawer);
-
-        elements.$pageContent.on("mouseup touchstart", contentClick);
-    }
+    // function setUIHandlers() {
+    // elements.$body.on("click", ".drawer-toggle", toggleDrawer);
+    // elements.$pageContent.on("mouseup", contentClick);
+    // }
 
     function activateTooltips() {
         $(".tooltips").tooltip({
@@ -28,45 +18,27 @@
         });
     }
 
-    function contentClick() {
-        //Improve so doesn't try to close drawers when none are open
-        hideDrawers();
-    }
-
-    // function toggleSideMenu(e) {
-    //   e.preventDefault();
-    //   elements.$html.toggleClass("menu-active");
-    //   elements.$sidebar.toggleClass("toggled");
+    // function contentClick() {
+    //     //Improve so doesn't try to close drawers when none are open
+    //     hideDrawers();
     // }
 
-    /**
-     * Adds hovered class to the menu icon of parent drop down
-     * Used to keep icon 'highlighted' when hovering on child elements
-     */
-    function menuDropdownHoverOn(e) {
-        $(this).closest(".dropdown").addClass("hovered");
-    }
+    // function toggleDrawer(e) {
+    //     e.preventDefault();
+    //
+    //     var drawerID = $(this).data("drawer");
+    //
+    //     var $drawer = $("#" + drawerID);
+    //
+    //     $(".drawer").not($drawer).removeClass("toggled");
+    //
+    //     $drawer.toggleClass("toggled");
+    // }
+    //
+    // function hideDrawers() {
+    //     elements.$drawers.removeClass("toggled");
+    // }
 
-    function menuDropdownHoverOff(e) {
-        $(this).closest(".dropdown").removeClass("hovered");
-    }
-
-    function toggleDrawer(e) {
-        e.preventDefault();
-
-        var drawerID = $(this).data("drawer");
-
-        var $drawer = $("#" + drawerID);
-
-        $(".drawer").not($drawer).removeClass("toggled");
-
-        $drawer.toggleClass("toggled");
-    }
-
-    function hideDrawers() {
-        elements.$drawers.removeClass("toggled");
-    }
-
-    setUIHandlers();
+    // setUIHandlers();
     activateTooltips();
 })();
--- js/script.js
+++ js/script.js
@@ -1,44 +1,14 @@
+var pageRegistry = [
+    'base',
+    'population',
+];
+
 function commaSeparateNumber(val) {
     while (/(\d+)(\d{3})/.test(val.toString())) {
         val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
     }
     return val;
 }
-
-/* --------------------------------------------------------
- Components
- -----------------------------------------------------------*/
-// (function () {
-//     //Popover
-//     $(".pover").popover({
-//         container: 'body'
-//     });
-// })();
-
-/* --------------------------------------------------------
- Animate numbers
- -----------------------------------------------------------*/
-// $(".quick-stats").each(function() {
-//   var target = $(this).find("h2");
-//   var toAnimate = $(this).find("h2").attr("data-value");
-//   // Animate the element's value from x to y:
-//   $({
-//     someValue: 0
-//   }).animate(
-//     {
-//       someValue: toAnimate
-//     },
-//     {
-//       duration: 1000,
-//       easing: "swing", // can be anything
-//       step: function() {
-//         // called on every step
-//         // Update the element's text with rounded-up value:
-//         target.text(commaSeparateNumber(Math.round(this.someValue)));
-//       }
-//     }
-//   );
-// });
 
 /**
  * Get the current page
@@ -50,7 +20,7 @@ function getPage() {
 
     var page = subpath.pop();
 
-    if (page == "main") {
+    if (page === "main") {
         var queryString = location.search.substring(1);
         page = queryString.split("/")[0];
     }
@@ -60,6 +30,10 @@ function getPage() {
 
 var page = getPage();
 
-$.getScript("gamestyles/default/js/pages/" + page + ".js").then(function () {
-    //   console.log(arguments);
-});
+if (pageRegistry.includes(page)) {
+    $.getScript("gamestyles/default/js/pages/" + page + ".js").then(function () {
+        //   console.log(arguments);
+    });
+}
+
+
